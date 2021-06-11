16ab92bd1cb9286241946acfbe1a458bf8531e13
Francesca Funghetti <48732708+francescafborn@users.noreply.github.com>
2021-06-11 19:44:58 -0300

    Add files via upload

---
+++ aula03.php
@@ -0,0 +1,46 @@
+<?php
+
+//transformador, tu coloca os valores e ele transforma em outros
+$valor = 10;
+$valor += 5;
+//echo $valor;
+
+//$nome = "francesca";
+//$nome .= "funghetti";
+//echo $nome;
+
+// == pra ver se é o mesmo valor
+//=== pra ver se é o mesmo valor e também o mesmo tipo de dado
+// != valores são diferentes?
+// !== dados diferentes e valores diferentes
+
+$valor1 = 10;
+$valor2 = 10;
+
+//var_dump($valor1 !== $valor2);
+
+//($valor1 == $valor2) ? $resultado = true : $resultado = false;
+//var_dump($resultado);
+/*
+$v1 = 10;
+$v1--;
+echo $v1;
+*/
+
+/*
+$v1 = 10;
+$v2 = $v1++; //++$v1 muda se tu soma o 1 antes ou depois de pegar a variavel
+echo $v1. '-' .$v2;
+*/
+
+// && significa "e", pra ver se são verdadeiros
+// || significa "ou", uma delas é verdadeira?
+// ! inverte a variavel que ta nos parenteses (true vira false, false vira true).
+
+$v1 = true;
+$v2 = false;
+//var_dump(!($v1));
+
+$nome = "francesca";
+$sobrenome = "funghetti";
+echo $nome.$sobrenome;
